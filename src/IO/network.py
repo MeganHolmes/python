78b132f707044bdc88ac3e8fa84e84ec56f54d29
Megan Holmes <meganholmes100@gmail.com>
2023-09-07 11:26:20 -0700

    Fix network functions

--- src/IO/network.py
+++ src/IO/network.py
@@ -2,23 +2,6 @@
 import socket
 import sys
 
-def broadcastMessageAndWaitForResponse(port, msg, waitTime):
-    broadcaster = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    broadcaster.bind(('', 0))
-    broadcaster.settimeout(waitTime)
-    broadcaster.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    # convert msg to bytes
-    broadcaster.sendto(msg.encode(), ('<broadcast>', port))
-
-    ret = (None, None)
-    try:
-        data, address = broadcaster.recvfrom(1024)
-        data = data.decode()
-        ret = (data, address)
-    except socket.timeout:
-        pass
-    broadcaster.close()
-    return ret
 
 def waitForMessage(port, waitTime):
     listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -34,14 +17,29 @@ def waitForMessage(port, waitTime):
     listener.close()
     return ret
 
+def waitForSocketConnection(port, waitTime):
+    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener.bind(('', port))
+    listener.settimeout(waitTime)
+    ret = None
+    try:
+        listener.listen(1)
+        ret = listener.accept()
+    except socket.timeout:
+        pass
+    except KeyboardInterrupt:
+        exit()
+    return ret
+
 def broadcastMessage(port, msg):
     broadcaster = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     broadcaster.bind(('', 0))
     broadcaster.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    broadcaster.sendto(msg, ('<broadcast>', port))
+    broadcaster.sendto(msg.encode(), ('<broadcast>', port))
     broadcaster.close()
 
-def establishConnection(host, port):
+def requestConnection(host, port, timeout):
+    socket.setdefaulttimeout(timeout)
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connection.connect((host, port))
     return connection
@@ -52,14 +50,7 @@ def sendString(connection, string):
 def receiveString(connection):
     return connection.recv(1024).decode()
 
-def checkIfConnected(connection):
-    try:
-        connection.sendall("".encode())
-        return True
-    except:
-        return False
-
-# I really wanted this to be in it's own test file but Python relative importing sucks.
+# I really wanted this to be in it's own test file but Python hates relative importing.
 def _test():
     mode = sys.argv[1]
     port = 21 # Port 21 is the default FTP port
@@ -70,24 +61,20 @@ def _test():
     while True:
         if mode == "server":
             print("Broadcasting")
-            data, address = broadcastMessageAndWaitForResponse(port, "Server Broadcast Test", timeout)
-            if data == "Client Response Test":
-                print(f"Client Responded from {address}. Attempting Socket Connection")
-                connection = establishConnection(address[0], port)
-
-                if checkIfConnected(connection):
-                    print("Connected to Client. Sending Server Test Message")
-                    sendString(connection, "Server Socket Test")
-                    data = receiveString(connection)
-                    if data == "Client Socket Test":
-                        print("Client Socket Test Successful")
-                        connection.close()
-                        broadcastMessage(port, "Server Broadcast Test Complete")
-                        break
-                    else:
-                        print("Client failed to respond")
+            broadcastMessage(port, "Server Broadcast Test")
+            connection = waitForSocketConnection(port, timeout)
+            if connection is not None:
+                client_socket, client_address = connection
+                print(f"Connected to Client {connection}. Sending Server Test Message")
+                sendString(client_socket, "Server Socket Test")
+                data = receiveString(client_socket)
+                if data == "Client Socket Test":
+                    print(f"Received {data}. Client Socket Test Successful")
+                    client_socket.close()
+                    broadcastMessage(port, "Server Broadcast Test Complete")
+                    break
                 else:
-                    print("Socket Connection Failed")
+                    print("Client failed to respond")
             else:
                 print("No Response")
         elif mode == "client":
@@ -95,14 +82,13 @@ def _test():
             data, address = waitForMessage(port, timeout)
             if data == "Server Broadcast Test":
                 print(f"Server Responded from {address}. Attempting Socket Connection")
-                connection = establishConnection(address[0], port)
-
-                if checkIfConnected(connection):
-                    print("Connected to Server. Sending Client Test Message")
+                connection = requestConnection(address[0], port, timeout)
+                if connection is not None:
+                    print(f"Connected to Server {connection}. Sending Client Test Message")
                     sendString(connection, "Client Socket Test")
                     data = receiveString(connection)
                     if data == "Server Socket Test":
-                        print("Server Socket Test Successful")
+                        print(f"Received {data}. Server Socket Test Successful")
                         connection.close()
                         broadcastMessage(port, "Client Response Test")
                         break
